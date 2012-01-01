587c8f07c172892f07448ffcceca08f2845bab86
Mika Tapojärvi <mika.tapojarvi@sse.fi>
2012-01-02 00:33:46 +0200

    Unittest script implemented, tests for creating and adding items implemented.

--- ecosldb/tests.py
+++ ecosldb/tests.py
@@ -23,57 +23,52 @@
 
 import sys
 import os
-import argparse
 from ecosldb import EcoDB
 import unittest
+import random
 
 
-class EcoDBTestSequence(unittest.TestCase):
-
-    self.dbfile='./unittest.db'
+class EcoDBTests(unittest.TestCase):
 
     def setUp(self):
-        print('creating a test database')
-        self.db = EcoDB(dbfile)
-        self.db.create_empty_database();
+        self.dbfile = dbfile
+        self.db = database
 
-    def tearDown(self):
-        print('removing used database DISABLED')
+    #def tearDown(self):
+        #print('removing used database DISABLED')
         #os.remove(dbfile)
 
-    def test_add_items(self):
-        print('adding items to database')
-        self.db.add_item({'maito,sin', 0})
+    def test_01_create_db(self):
+        if not os.path.exists(self.dbfile) or not os.path.isfile(self.dbfile):
+            database.create_empty_database();
+
+    def test_02_add_items(self):
+        items = ['täysmaito',
+            'rasvatonmaito',
+            'kevytmaito',
+            'ruisleipä',
+            'ranskanleipä',
+            'paahtoleipä',
+            'Keiju-margariini',
+            'Flora-margariini',
+            'Oivariini',
+            'leivontamargariini',
+            'Pirkka-kahvi',
+            'Saludo-kahvi',
+            'Presidentti-kahvi']
+
+        for an_item in items:
+            self.db.add_item([an_item, 0])
+
 
 if __name__ == '__main__':
     """"Main function."""
 
-    # Main parser
+    dbfile='./unittest.db'
+    database = EcoDB(dbfile)
 
-    ap = argparse.ArgumentParser()
-    ap.add_argument('-i', '--items', action='store_true', help='test for adding and modifying items.')
-    
-    #create_parser = subparsers.add_parser('create', help='subcommand to create a new database or dump the contents to a file.');
-    #create_parser.add_argument('-e', '--empty', action='store_true', help='Create a new, empty database.')
-    #create_parser.add_argument('-f', '--file', nargs=1, metavar='<path/file.sql>', dest='inputfile', help='Create a new database and import contents from <path/file.sql>.')
-    #create_parser.add_argument('-d', '--dump', nargs=1, metavar='<path/file.sql>', dest='dumpfile', help='Create a dump of database contents to <path/file.sql>.')
-    #add_parser.add_argument('--item', nargs=2, metavar=('"<name>"', '<list id>'), help='Add new item <name>. <list id> is either a shopping list id or 0, which means the item available for all lists.')
-    #add_parser.add_argument('--lang', nargs=1, metavar='"<language>"', help='Add new language for item translations.')
-    #add_parser.add_argument('--tr', nargs=3, metavar=('<item id>', '<language id>', '"<translation>"'), dest='translation', help='Add new translation for an item <item id> to language <language id>. Translated string is "<translation>".')
-
-
-    args = ap.parse_args()
-
-    print(args) #  debug
-
-
-
-    # Arguments are parsed, do the required tasks.
-
-    # item tests
-    if hasattr(args, 'items'):
-        print('testing items')
-        unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(EcoDBTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
 
 
 
